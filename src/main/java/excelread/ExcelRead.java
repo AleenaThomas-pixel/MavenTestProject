0e2fb084da521c9d4ce8633d2f25b085e464ceb0
AleenaThomas-pixel <aleenathom1996@gmail.com>
2024-10-18 17:02:46 +0300

    new changes

--- src/main/java/excelread/ExcelRead.java
+++ src/main/java/excelread/ExcelRead.java
@@ -15,8 +15,8 @@ public class ExcelRead {
 	
 	ExcelRead() throws IOException {
 	
-	FileInputStream inputfile = new FileInputStream("C:\\Users\\Monu Thomas M\\Documents");
-	XSSFWorkbook workbook = new   XSSFWorkbook (inputfile);//HSSFWorkbook .xls
+	FileInputStream inputfile = new FileInputStream("D:\\Aleena\\Empdetails.Worksheet.xlsx");
+	XSSFWorkbook workbook = new   XSSFWorkbook (inputfile);//HSSFWorkbook if file type is .xls
 	
 	sheet = workbook.getSheet("Sheet1");
 	}
@@ -43,17 +43,17 @@ public static void main(String args[]) throws IOException {
 	
 	ExcelRead ex = new ExcelRead();
 	
-	//int r = ex.sheet.getLastRowNum();
-	//ex.sheet.getRow(r).getLastCellNum();
+	int r = ex.sheet.getLastRowNum();
+	ex.sheet.getRow(r).getLastCellNum();
+	
 	System.out.println("Excel DATA : ");
 	for (int i=0; i< ex.sheet.getLastRowNum()+1;i++) {
 		for (int j=0; j<= ex.sheet.getRow(i).getLastCellNum()-1;j++) {
 			
-			
 			String s = ex.readExcel(i,j);
-			
-			System.out.println(s + "  ");
+			System.out.print(s + "  ");
 		}
+		System.out.println();
 	}
 	
 }
